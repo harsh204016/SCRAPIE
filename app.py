889c24685c5aa22587bb863275747345e49003bd
kanchi12P <kanchipardhi@gmail.com>
2022-10-28 20:45:29 +0530

    added tips page

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from flask import Flask, render_template, request
 from edamam import recipe_search
 
-# create an instance of the Flask class
-# name is the place holder for current module, here is app.py
 app = Flask(__name__)
 app.config.from_object('config')
 app_id = app.config["APP_ID"]
 app_key = app.config["APP_KEY"]
 
-# use the route decorator to tell Flask what URL should trigger our function.
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -25,6 +22,10 @@ def show_recipe():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/tips')
+def tips():
+    return render_template('tips.html')
 
 # it checks if a module is being imported or not
 if __name__ == '__main__':
